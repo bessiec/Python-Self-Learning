5e225901f1fbdcaa8549eb549cbf6b2e2ac97797
Bessie Chu <oysteromelette@Bessie-Chus-MacBook-Pro.local>
2013-07-05 22:45:47 -0700

    7.5.13 11pm commit

---
+++ Exercises/ex12.py
@@ -0,0 +1,10 @@
+#Learn Python the Hard Way Exercise 12: Prompting People
+
+from sys import argv
+
+script, blank, chicken = argv
+
+print "The script is called:", script
+print "Your first variable is:", blank
+print "Your second variable is:", chicken
+
---
+++ Exercises/ex9.py
@@ -0,0 +1,17 @@
+#Learn Python the Hard Way Exercise 9: Printing, Printing, Printing
+
+days = "Mon Tues Weds Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+print "Here are the %r: " % months
+
+print """
+
+There's something going on here.  
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
+
