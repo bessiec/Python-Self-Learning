bcc1348bd9efe1cd5f6ebe33803581f587642bc5
bessiec <chu.bessie@gmail.com>
2013-09-21 14:31:00 -0700

    Hackbright Review/Guit Coding

--- wordcount.py
+++ wordcount.py
@@ -1,21 +1,26 @@
 #Hackbright Exercise 6 Redux/Review in funemployment grad school time
 #https://github.com/chriszf/Hackbright-Curriculum/tree/master/Exercise06
 
-
 from sys import argv
+import string
+import re
 script, filename = argv
 
-f = open(filename).read().lower() #open and read the file, makes it into one string lowercase
-f.close() #close the file
-f_split = f.split() #split the string into a list
+text = open(filename).read().lower() #open and read the file, makes it into one string lowercase
 
-f_dict = {} #initialize a dictionary
+text = re.sub("["+string.punctuation+"]", '', text)
 
-for index in range(len(f_split)):
-	#loop over range of length of list 
-	word = f_split[index]
-	f_split[index] = word.strip('.')
-	#reassign the striped word back into f_split
 
-for f in f_split:
-	f_dict[word] = f_dict.get(word, 0) + 1 #fun with tuples to see if key exists, if it doesn't, return 0
+word_count = {}
+
+for word in text:
+	word_count[word] = word_count.get(word, 0) +1 
+
+for word, num in word_count.iteritems():
+	print "The word %s occurs %d times." % (word, num) 
+
+
+word_count_ordered = sorted(word_count.iteritems(), key = lambda word: (word[1], word[0]), reverse = False)
+
+for word, num in word_count_ordered:
+	print "The word %s occurs %d times." % (word, num) 
