1f6467584a5ac7e34ddef3413ec4750ddfb6fd52
bessiec <chu.bessie@gmail.com>
2013-10-04 22:58:27 -0700

    Fixed some bugs.  Months later, basic syntax still hard, but the coding practice must go on

--- CodeAcademy/battleship.py
+++ CodeAcademy/battleship.py
@@ -24,32 +24,33 @@ def random_col(board):
 def random_row(board):
     return randint(0, len(board[0]) -1) #we do this because board starts 0, 1, 2... 
 
+#placing the battleship
 ship_row = random_row(board)
 ship_col = random_col(board)
 
-#asking user to guess a row and column to sink the ship
-guess_row = int(raw_input("Guess Row:"))
-guess_col = int(raw_input("Guess Col:"))
-
 #player actions
-turn = 0
 for turn in range(4): #four chances to guess
     guess_row = int(raw_input("Guess Row:"))
     guess_col = int(raw_input("Guess Col:"))
 
     if guess_row == ship_row and guess_col == ship_col:
         print "Congratulations! You sunk my battleship!"
+        print_board(board)
         break
     else:
         if guess_row < 0 or guess_row > len(board)-1 or guess_col < 0 or guess_col > len(board[0]) - 1:
             print "Oops, that's not even in the ocean."
-        elif(board[guess_row][guess_col] == "X"):
+            print_board(board)
+        elif board[guess_row][guess_col] == "X":
             print "You guessed that one already."
+            print_board(board)
         else:
             print "You missed my battleship!"
             board[guess_row][guess_col] = "X"
-            if turn == 3:
-                print "Game Over"
+            print_board(board)
     print "You taken %s turns" % str(turn+1)
+    if turn == 3:
+            print "Game Over"
+  
 
 
